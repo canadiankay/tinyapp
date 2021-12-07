5d23c3f1d333a745defe30463c1593215e86c5a5
canadiankay <kasimnadia@gmail.com>
2021-12-07 04:52:44 +0000

    filling out the urls_index template to display a chart iwth short and long URLS

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080; // default port 8080
 //// tells the Express app to use EJS as its templating/ 'view' engine.
 app.set('view engine', 'ejs');
 
+//this object is used to keep track of all the shortened URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,10 +19,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 }); //this will print a JSON string representing the urlDatabase Object
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 //response can contain HTML code, which would be rendered in the client browser.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
