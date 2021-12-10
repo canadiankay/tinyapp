28f17341276aec5ad823aa31caede299eba952f6
canadiankay <kasimnadia@gmail.com>
2021-12-10 00:28:23 +0000

    readded shortened version of header to login and register (managed to figure out a workaround), and added a helper function for finding existing user via their email in the database so we can reuse

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 /* MY NOTES
 res.render ("name of template", an object with a bunch of key/value pairs) so that in our template, we can access each of these key/value pairs 
-//if you have the urls it'll be on the index page
+if you have the urls it'll be on the index page
+setCookie has two parameters (cookie name, the value you want associated to the cookie )
+clearCookies takes one parameter (cookie name)
+instead of doing console log of the user database, we can alternatively do domain'/users.json' as that will list them all as well
+since the header is shown across all of the ejs pages, and we use the variable 'user' in the header, we need user to be in every single app.get that renders these pages
+//therefore need a templateVars everywhere with the user key anywhere we are rendering a file
 
 */
 const PORT = 8080;
@@ -45,6 +50,19 @@ const generateRandomString = function(length=6){
 };
 console.log(generateRandomString());
 
+//handle registration error - if email already exists, do not re-register them
+//function that will look at the email and scroll through the users object database
+const findUserByEmail = (email, userDatabase) => {
+  // for (let key in database)
+  for (let id in userDatabase) {
+    const user = users[id]; // => retrieve the value that's in id 
+
+    if (user.email === email) { // check if email from form === email from the database
+      return user;
+    }
+  }
+    return false;
+};
 
 //------------------------------------------------------END POINTS & ROUTES ----------------------------->
 app.get("/", (req, res) => {
@@ -82,9 +100,12 @@ app.post("/urls", (req, res) => {
 
 //this will render/create the page with the form and show it to the client/user
 app.get("/urls/new", (req, res) => {
+  //get user id from cookie
   const user_id = req.cookies.user_id
   //const templateVars = {username: req.cookies["username"]}; //added this so that username at the top still shows in header
-  const templateVars = {user: users[user_id]}; //added this so that username at the top still shows in header
+  const templateVars = {
+    user: users[user_id]
+  }; //added this so that username at the top still shows in header
   res.render("urls_new", templateVars); 
 });
 
@@ -106,8 +127,9 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]; //object containing properties mapped to a named route parameters//the long URL will look for its shortURL key in the database
   const user_id = req.cookies.user_id
   //const templateVars = {username: req.cookies["username"]}
-  const templateVars = {user: users[user_id]}
-
+  const templateVars = {
+    user: users[user_id]
+  };
   //res.render("urls_show", templateVars);
   //redirect to longURL
   res.redirect(longURL, templateVars);
@@ -123,14 +145,100 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+//----------------------------------------------Authentication Routes -------------------->
+// --------------------------REGISTRATION-------------------> 
+app.get("/register", (req, res) => { //endpoint
+  const templateVars = {
+    user: null //since we haven't logged in yet here, user would be null here
+  };
+  res.render("register", templateVars); // render registration page 
+  res.status(404);
+});
 
-//------------ CREATE/POST --------------------- these two are found in _header and they allow for login/logout and displays login when a person enters username
+
+//receive info from the registration page- registration handler
+app.post("/register", (req, res) => { //when I submit register form I want the info to be receive that info from
+  
+  // Extract the user info from the incoming form after client clicks register -- using req.body (body parser of express)
+  const email = req.body.email; //this matches the email attribute form the register form
+  const password = req.body.password; 
+
+  //handle registration errors - if email and/or password are blank
+  if (email === "" || password === "") {
+    return res.status(400).send("Please enter a valid email address and/or password");
+  }
+  //---------------------------------------------------------------- MOVED TO HELPER FUNCTIONS B/C WE REUSE THIS IN LOGIN
+  // //function that will look at the email and scroll through the users object database
+  // const findUserByEmail = (email, users) => {
+  // // for (let key in database)
+  //   for (let id in users) {
+  //     const user = users[id]; // => retrieve the value that's in id 
+  //     if (user.email === email) {
+  //       return user;
+  //     }
+  //   }
+  //   return false;
+  // };
+
+  //handle registration errors - if email already exists-- use function we created above ---- moved to helper functions
+    
+    const user = findUserByEmail(email,users);
+    // ^we want to find the user using their email through the usersdb
+
+    if (user) { //if user already exists then no need to create a new user
+      res.status(403).send('Sorry, user already exists!');
+      return;
+    }
+
+  //generate a new user id
+  const id = generateRandomString();
+
+  //create new user AND add their name, email, password to our users database
+  const newUser = { //This endpoint should add a new user object to the global users object
+    id: id,
+    email: email,
+    password: password
+  }
+
+  // add the new user to our users obj database (i.e. we need to ascribe it to a key value and in our case the random generated string)
+  users[id] = newUser //we want it to be equal to new user object above 
+
+  //set the cookie-- we want to the browser keep the user id in the cookie
+  res.cookie("user_id", id); //test cookie in browswer
+
+  //redirect to '/urls'
+  res.redirect("/urls");
+});
+
+//----------------------------------------------------------------LOGIN ---------->
 // temporaray route to show all the users in the users database
 app.get('/users.json', (req, res) => {
   res.json(users);
 });
 
+app.get("/login", (req, res) => { //endpoint-- render registration page 
+  const templateVars = {
+    user: null //since we haven't logged in yet here, user would be null here
+  };
+  res.render("login", templateVars);
+});
+
+//authenticate the user 
 app.post("/login", (req, res) => {
+  //extract the user info from the form
+  const email = req.body.email; //this matches the email attribute form the register form
+  const password = req.body.password; 
+
+  //chcek if user exists-- use for in loop that we made previously to check if email is there
+
+
+
+
+
+
+
+
+
   const user_id = req.cookies.user_id
   //const username = req.body.username; //whatever gets entered will be stored here
   user: users[user_id]
@@ -141,79 +249,12 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   //const username = req.body.username; //whatever gets entered will be stored here
   const user_id = req.cookies.user_id
+
+  //clear cookie
   //res.clearCookie("username",username); // clear cookie with that username
-  res.clearCookie("user_id", user_id); 
+  res.clearCookie("user_id"); 
   res.redirect("/urls");
 });
-
-
-//----------------------------------------------Authentication Routes -------------------->
-// --------------------------REGISTRATION-------------------> 
-app.get("/register", (req, res) => { //endpoint
-  res.render("register"); // render registration page 
-  res.status(404);
-});
-
-
-//receive info from the registration page- registration handler
-app.post("/register", (req, res) => { //when I submit register form I want the info to be receive that info from
-  
-  // Extract the user info from the incoming form after client clicks register -- using req.body (body parser of express)
-  const email = req.body.email;
-  const password = req.body.password; 
-
-  //handle registration errors - if email and/or password are blank
-  if (email === "" || password === "") {
-    return res.status(400).send("Please enter a valid email address and/or password");
-  }
-  //function that will look for email in the users object
-  const findUserByEmail = (email, users) => {
-    for (let id in users) {
-      const user = users[id]; // => retrieve the value
-      if (user.email === email) {
-        return user;
-      }
-    }
-    return false;
-  };
-
-
-  //handle registration errors - if email already exists
-    //we want to check if email =req.body.email === email from the database
-    const user = findUserByEmail(email,users);
-    if (user) {
-      res.status(403).send('Sorry, user already exists!');
-      return;
-    }
-
-  //create/generate a new user id
-  const id = generateRandomString();
-
-  //add name, email, password to our users db => create a new user
-  const newUser = { //This endpoint should add a new user object to the global users object
-    id: id,
-    email: email,
-    password: password
-  }
-  console.log(`${newUser}: users`);
-
-  // add the new user to the  users object db 
-    users[id] = newUser;
-
-
-  //ask browser to set cookie for us that will be attached to a userid
-  res.cookie("user_id", id); //test cookie in browswer
-
-  //redirect to '/urls'
-  res.redirect("/urls");
-});
-
-//----------------------------------------------------------------LOGIN ---------->
-app.get("/login", (req, res) => { //endpoint-- render registration page 
-  res.render("login");
-});
-
-
 
 
 //------------------ DELETE-------------------------------
