ae9d73405d008310be835db649457f80c4ed5956
canadiankay <kasimnadia@gmail.com>
2021-12-13 00:46:26 +0000

    pushing eveyrthing I have thus far before writing tests

--- express_server.js
+++ express_server.js
@@ -26,7 +26,7 @@ app.use(cookieSession({
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
-    userID: "userRandomID"
+    userID: "aJvfe3"
   },
   "9sm5xK": {
     longURL: "http://www.google.com",
