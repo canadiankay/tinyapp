0aa3c41ededabf6088be4442a7bcb0c799b0232d
canadiankay <kasimnadia@gmail.com>
2021-12-13 01:24:37 +0000

    this includes the mocha tests-- everything works yay!

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { findUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,10 +15,24 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function() {
+describe('findUserByEmail', function() {
+
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
-    const expectedUserID = "userRandomID";
-    
+    const user = findUserByEmail("user@example.com", testUsers)
+    const expectedUserID = testUsers.userRandomID;;
+    assert.equal(user, expectedUserID)
   });
+
+  
+  it('should return null/undefined if the email isnt in the database', function() {
+    const user = findUserByEmail("invalid@example.com", testUsers);
+    const expectedResult = undefined;
+    assert.equal(user, expectedResult);
+  });
+  
+  
+
+
+
+
 });
